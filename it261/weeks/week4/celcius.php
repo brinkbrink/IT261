5fe16a36e89cdd867174aeddd7bbd2b86b12099d
brinkbrink <brinkbrink@gmail.com>
2021-10-25 14:48:04 -0700

    it 261 hw

---
+++ it261/weeks/week4/celcius.php
@@ -0,0 +1,8 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Week 4 - Celcius Form</title>
+<link href="css/styles.css" type="text/css" rel="stylesheet">
+</head>
+<body>
