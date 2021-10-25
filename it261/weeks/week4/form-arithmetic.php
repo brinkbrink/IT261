081bda869144c6b16ebd6f46c8c9b9301f4cfc81
brinkbrink <brinkbrink@gmail.com>
2021-10-25 14:40:37 -0700

    dfssd

--- it261/weeks/week4/form-arithmetic.php
+++ it261/weeks/week4/form-arithmetic.php
@@ -2,32 +2,39 @@
 <html lang="en">
 <head>
 <meta charset="UTF-8">
-<title>Form No. 3, featuring HTML and CSS</title>
+<title>Arithmetic Form</title>
+
 <style>
 form {
     width: 400px;
     margin: 0 auto;
 }
+
 fieldset {
     border: 1px solid red;
     padding: 10px;
 }
+
 label, textarea {
     display: block;
     margin-bottom: 5px;
 }
+
 input, textarea {
     margin-bottom: 10px;
 }
+
 input [type=text], 
 input [type=email], 
 textarea {
     width: 90%;
 }
+
 h3 {
     color: red;
     text-align: center;
 }
+
 .box { 
     width: 400px;
     margin: 20px auto;
@@ -36,37 +43,44 @@ h3 {
 }
 
 </style>
-
 </head>
+
 <body>
 <form action="" method="post">
     <fieldset>
         <label for="fname">First Name</label>
         <input type="text" name="fname">
-        <label for="lname">Last Name</label>
-        <input type="text" name="lname">
-        <label for="email">Email</label>
-        <input type="email" name="email">
-        <label for="comments">Comments</label>
-        <textarea name="comments"></textarea>
+        <label for="brushes">How many paint brushes?</label>
+        <input type="number" name="textbooks">
+        <label for="quills">How many quills?</label>
+        <input type="number" name="quills">
+        <label for="pencils">How many pencils?</label>
+        <input type="number" name="pencils">
         <input type="submit" value="Send it!">
     </fieldset>
 </form>
+
 <?php
 // form 3 - adding the HTML and CSS!
 if(isset($_POST['fname'],
-        $_POST['lname'],
-        $_POST['email'],
-        $_POST['comments'])) {
+        $_POST['brushes'],
+        $_POST['quills'],
+        $_POST['pencils'])) {
 
 $fname = $_POST['fname'];
-$lname = $_POST['lname'];
-$email = $_POST['email'];
-$comments = $_POST['comments'];
+$brushes = $_POST['brushes'];
+$int_brushes = intval($brushes);
+$quills = $_POST['quills'];
+$int_quills = intval($quills);
+$pencils = $_POST['pencils'];
+$int_pencils = intval($pencils);
+
+$total = $int_brushes + $int_quills + $int_pencils;
+
 
 // we are going to nest an if/else statement
 
-if(empty($fname && $lname && $email && $comments)) {
+if(empty($fname && $int_brushes && $int_quills && $int_pencils)) {
     echo '<h3>Please fill out all inputs</h3>';
 } else {
     // echo $fname;
@@ -76,12 +90,14 @@ if(empty($fname && $lname && $email && $comments)) {
 
     echo '
     <div class="box">
+    <h2>Hello '.$fname.'</h2>
+    <p>You have collected the following:</p>
     <ul>
-    <li><b>First Name:</b> '.$fname.'</li>
-    <li><b>Last Name:</b> '.$lname.'</li>
-    <li><b>Email:</b> '.$email.'</li>
-    <li><b>Comments:</b> '.$comments.'</li>
+    <li>'.$int_brushes.' Brushes</li>
+    <li>'.$int_quills.' Quills</li>
+    <li>'.$int_pencils.' Pencils</li>
     </ul>
+    <p>Totalling '.$total.' beverages</p>
     </div>
     ';
 
