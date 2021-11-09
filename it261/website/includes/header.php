d9d74edd7d92044f7843a57bf522eece18c8446a
brinkbrink <brinkbrink@gmail.com>
2021-11-08 20:58:42 -0800

    week 6

--- it261/website/includes/header.php
+++ it261/website/includes/header.php
@@ -1,60 +1,3 @@
-<?php
-
-define('THIS_PAGE', basename($_SERVER['PHP_SELF']));
-
-$nav['index.php'] = 'Home';
-$nav['about.php'] = 'About';
-$nav['daily.php'] = 'Daily';
-$nav['project.php'] = 'Project';
-$nav['contact.php'] = 'Contact';
-$nav['gallery.php'] = 'Gallery';
-
-switch(THIS_PAGE) {
-    case 'index.php':
-    $title = 'Home page of our IT 261 Website';
-    $body = 'home';
-    $headline = 'Welcome to the Home page of our IT 261 Website';
-    break;
-
-    case 'about.php':
-    $title = 'About page of our IT 261 Website';
-    $body = 'about inner';
-    $headline = 'Welcome to the About page of our IT 261 Website';
-    break;
-
-    case 'daily.php':
-    $title = 'Daily page of our IT 261 Website';
-    $body = 'daily inner';
-    $headline = 'Welcome to the Daily page where my Homework 3 Switch will display';
-    break;
-
-    case 'project.php':
-    $title = 'Project page of our IT 261 Website';
-    $body = 'project inner';
-    $headline = 'Welcome to the Project page of our IT 261 Website';
-    break;
-
-    case 'contact.php':
-    $title = 'Contact page of our IT 261 Website';
-    $body = 'contact inner';
-    $headline = 'Welcome to the Contact page of our IT 261 Website';
-    break;
-
-    case 'gallery.php':
-    $title = 'Gallery page of our IT 261 Website';
-    $body = 'gallery inner';
-    $headline = 'Welcome to the Gallery page of our IT 261 Website';
-    break;
-}
-
-// beginning of switch for hw 3
-if(isset($_GET['today'])) {
-    $today = $_GET['today'];
-    } else {
-    $today = date('l');
-    }
-?>
-
 <!doctype html>
 <html lang="en">
 <head>
@@ -83,19 +26,7 @@ if(isset($_GET['today'])) {
 
             <ul>
             <?php
-            foreach($nav as $key => $value){
-                if(THIS_PAGE == $key) {
-                    echo '
-                    <li><a class ="current" href="'.$key.'">'.$value.'</a></li>
-                    ';
-                } else {
-                    echo ' 
-                    <li><a href="'.$key.'">'.$value.'</a></li>
-                    ';
-                }
-
-            }
-
+            echo my_nav($nav);
             ?>
             </ul>
         </nav>
