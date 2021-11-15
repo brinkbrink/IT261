32b3d74c3be831f1042c72783885b6be8c20d226
brinkbrink <brinkbrink@gmail.com>
2021-11-15 14:16:29 -0800

    week7

--- it261/website/index.php
+++ it261/website/index.php
@@ -23,7 +23,7 @@ echo rand_pic($photos);
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nec ipsum ut augue varius ornare tempor et leo. Integer rhoncus condimentum mi, in porta urna sollicitudin eu. In lectus purus, gravida id tortor eget, iaculis aliquam enim. Maecenas sagittis dapibus metus, non blandit lectus. Vestibulum vel ipsum molestie, elementum nibh a, pellentesque tellus. Aenean venenatis ultrices ligula vitae blandit. Morbi rhoncus bibendum euismod. Vivamus congue mauris nisi, at cursus velit euismod a. Mauris id lectus sagittis, imperdiet nisl non, gravida diam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc aliquet orci vel lacus mollis faucibus.</p>
     </main>
     <aside>
-    <h3>Here is my aside</h3>
+    <h3><img alt="Mount Tahoma" src="images/tahoma.png"></h3>
     </aside>
 
 <?php
