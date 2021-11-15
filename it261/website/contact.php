eb6347f12018e2f1177e6bd73901f146106306a1
brinkbrink <brinkbrink@gmail.com>
2021-11-15 11:28:32 -0800

    week7

---
+++ it261/website/contact.php
@@ -0,0 +1,16 @@
+<?php
+include('config.php');
+include('includes/header.php');?>
+
+    <div id="wrapper">
+    <main>
+    <h1><?php echo $headline; ?></h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nec ipsum ut augue varius ornare tempor et leo.</p>
+    <h2>Please fill out our form!</h2>
+    </main>
+    <aside>
+    <h3>Here is my aside for our form</h3>
+    </aside>
+
+<?php
+include('includes/footer.php');?>
--- it261/website/index.php
+++ it261/website/index.php
@@ -1,5 +1,5 @@
 <?php
-include('includes/config.php');
+include('config.php');
 include('includes/header.php');?>
 
     <div id="wrapper">
@@ -11,7 +11,7 @@ include('includes/header.php');?>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nec ipsum ut augue varius ornare tempor et leo. Integer rhoncus condimentum mi, in porta urna sollicitudin eu. In lectus purus, gravida id tortor eget, iaculis aliquam enim. Maecenas sagittis dapibus metus, non blandit lectus. Vestibulum vel ipsum molestie, elementum nibh a, pellentesque tellus. Aenean venenatis ultrices ligula vitae blandit. Morbi rhoncus bibendum euismod. Vivamus congue mauris nisi, at cursus velit euismod a. Mauris id lectus sagittis, imperdiet nisl non, gravida diam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc aliquet orci vel lacus mollis faucibus.</p>
     </main>
     <aside>
-
+    <h3>Here is my aside</h3>
     </aside>
 
 <?php
